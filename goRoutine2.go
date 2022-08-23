b091447dc24ad706023e963cd12eee39cf346134
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2022-08-24 00:22:24 +0900

    add goRoutine practice2

--- goRoutine2.go
+++ goRoutine2.go
@@ -6,6 +6,21 @@ import (
 )
 
 func main() {
+
+	goRoutine1()
+
+	fmt.Println("")
+
+	goRoutine2()
+
+}
+
+func process(name string) {
+	time.Sleep(2 * time.Second)
+	fmt.Println(name)
+}
+
+func goRoutine1() {
 	fmt.Println("START")
 
 	//boolean型のchannel作成
@@ -22,4 +37,29 @@ func main() {
 
 	fmt.Println(isFin)
 	fmt.Println("FINISH")
+}
+
+func goRoutine2() {
+	isFin1 := make(chan bool)
+	isFin2 := make(chan bool)
+	isFin3 := make(chan bool)
+
+	fmt.Println("START2")
+	go func() {
+		process("A")
+		isFin1 <- true
+	}()
+	go func() {
+		process("B")
+		isFin2 <- true
+	}()
+	go func() {
+		process("C")
+		isFin3 <- true
+	}()
+
+	<-isFin1
+	<-isFin2
+	<-isFin3
+	fmt.Println("FINISH2")
 }
