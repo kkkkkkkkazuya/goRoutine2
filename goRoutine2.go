8b02727d1c9a156668e9a40a66646ad7f9ab5655
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2022-08-24 23:57:30 +0900

    add goRoutine Hard

--- goRoutine2.go
+++ goRoutine2.go
@@ -2,64 +2,92 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"testing"
 	"time"
 )
 
 func main() {
 
-	goRoutine1()
+	// goRoutine1()
 
-	fmt.Println("")
+	// fmt.Println("")
 
-	goRoutine2()
+	// goRoutine2()
+
+	result := testing.Benchmark(func(b *testing.B) { run("A", "B", "C", "D", "E") })
+	fmt.Println(result.T)
 
 }
 
-func process(name string) {
+func run(name ...string) {
+	fmt.Println("START")
+	wg := new(sync.WaitGroup)
+	siFin := make(chan bool, len(name))
+
+	for _, v := range name {
+		//Max5で処理一つに対して、1増やす
+		wg.Add(1)
+		go process(v, siFin, wg)
+	}
+
+	wg.Wait()
+	close(siFin)
+	fmt.Println("Finish")
+}
+
+func process(name string, isFin chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
 	time.Sleep(2 * time.Second)
 	fmt.Println(name)
+	isFin <- true
 }
 
-func goRoutine1() {
-	fmt.Println("START")
+// func process1(name string) {
+// 	time.Sleep(2 * time.Second)
+// 	fmt.Println(name)
+// }
 
-	//boolean型のchannel作成
-	ch := make(chan bool)
+// func goRoutine1() {
+// 	fmt.Println("START")
 
-	//goroutine作成
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch <- true
-	}()
+// 	//boolean型のchannel作成
+// 	ch := make(chan bool)
 
-	isFin := <-ch
-	close(ch)
+// 	//goroutine作成
+// 	go func() {
+// 		time.Sleep(2 * time.Second)
+// 		ch <- true
+// 	}()
 
-	fmt.Println(isFin)
-	fmt.Println("FINISH")
-}
+// 	isFin := <-ch
+// 	close(ch)
 
-func goRoutine2() {
-	isFin1 := make(chan bool)
-	isFin2 := make(chan bool)
-	isFin3 := make(chan bool)
+// 	fmt.Println(isFin)
+// 	fmt.Println("FINISH")
+// }
 
-	fmt.Println("START2")
-	go func() {
-		process("A")
-		isFin1 <- true
-	}()
-	go func() {
-		process("B")
-		isFin2 <- true
-	}()
-	go func() {
-		process("C")
-		isFin3 <- true
-	}()
+// func goRoutine2() {
+// 	isFin1 := make(chan bool)
+// 	isFin2 := make(chan bool)
+// 	isFin3 := make(chan bool)
 
-	<-isFin1
-	<-isFin2
-	<-isFin3
-	fmt.Println("FINISH2")
-}
+// 	fmt.Println("START2")
+// 	go func() {
+// 		process1("A")
+// 		isFin1 <- true
+// 	}()
+// 	go func() {
+// 		process1("B")
+// 		isFin2 <- true
+// 	}()
+// 	go func() {
+// 		process1("C")
+// 		isFin3 <- true
+// 	}()
+
+// 	<-isFin1
+// 	<-isFin2
+// 	<-isFin3
+// 	fmt.Println("FINISH2")
+// }
